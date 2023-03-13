47caf3a3e84eeaf03ebf4c844d70a72cec869551
HarlockOfficial <harlockofficial.github@gmail.com>
2023-03-13 14:25:29 +0100

    [edit] logger

--- logger/logger.py
+++ logger/logger.py
@@ -1,5 +1,10 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%[(asctime)s] %(filename)s %(levelname)s: %(message)s',
+
+logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(levelname)s: %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger('')
 logger.setLevel(logging.DEBUG)
+
+
+def log(msg, level=logger.info):
+    level(msg)
