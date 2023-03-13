b025e164e82fb9e8c38a3d82b883802472d31aff
HarlockOfficial <harlockofficial.github@gmail.com>
2023-03-13 17:27:59 +0100

    [add] initial behaviour of host cell

--- Agents/HostCell.py
+++ Agents/HostCell.py
@@ -11,16 +11,21 @@ class HostCell(BaseAgent):
         """
             write agent behaviour here
         """
-        pass
+        if self.current_infection > 0:
+            self.current_infection -= 0.1
 
     def animation(self, index):
         """
             write agent animation behaviour here
         """
-        pass
+        if self.current_infection > 0:
+            self.graphic_component.set_color(BaseAgent.get_colour_name((255, 0, 0)))
+        else:
+            self.graphic_component.set_color(BaseAgent.get_colour_name((255, 255, 255)))
 
     def __init__(self):
         BaseAgent.__init__(self, color=(255, 255, 255), shape=AgentShape.CUBE)
         import json
         pos = [self.position[0], self.position[1], self.position[2]]
         logger.log("host cell;" + str(id(self)) + ";" + str(json.dumps(pos)))
+        self.current_infection = 0.0
